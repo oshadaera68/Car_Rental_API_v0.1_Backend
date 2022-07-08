0cabd2afd29055dd8d86693f21aa10835b9f6627
oshadaera68 <mindartlk010@gmail.com>
2022-07-08 11:12:37 +0530

    JPAConfig class finished

--- src/main/java/lk/ijse/Car_Rental_Sys_api/config/JPAConfig.java
+++ src/main/java/lk/ijse/Car_Rental_Sys_api/config/JPAConfig.java
@@ -5,5 +5,46 @@
 
 package lk.ijse.Car_Rental_Sys_api.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class JPAConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va){
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setJpaVendorAdapter(va);
+        bean.setDataSource(ds);
+        bean.setPackagesToScan("lk.ijse.Car_Rental_Sys_api.entity");
+        return bean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/car_rental?createDatabaseIfNotExist=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter vendorAdapter(){
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        vendorAdapter.setDatabase(Database.MYSQL);
+        vendorAdapter.setShowSql(true);
+        vendorAdapter.setGenerateDdl(true);
+        return vendorAdapter;
+    }
+
 }
