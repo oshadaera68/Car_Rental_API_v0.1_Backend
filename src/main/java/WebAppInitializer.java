ab2e60dbf0b069d5a156c4830a3d5d33e02dadbd
oshadaera68 <mindartlk010@gmail.com>
2022-07-08 10:18:35 +0530

    updated content

--- src/main/java/WebAppInitializer.java
+++ src/main/java/WebAppInitializer.java
@@ -1,7 +1,25 @@
+import lk.ijse.Car_Rental_Sys_api.config.WebAppConfig;
+import lk.ijse.Car_Rental_Sys_api.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @Owner - Oshada Eranga
  * @Version - v0.1.0
  **/
 
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
