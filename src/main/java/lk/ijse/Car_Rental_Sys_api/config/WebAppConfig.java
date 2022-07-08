5c9d0cd12b02e23bdc925e30d74005929829faab
oshadaera68 <mindartlk010@gmail.com>
2022-07-08 11:15:34 +0530

    updated webappconfig classes

--- src/main/java/lk/ijse/Car_Rental_Sys_api/config/WebAppConfig.java
+++ src/main/java/lk/ijse/Car_Rental_Sys_api/config/WebAppConfig.java
@@ -5,5 +5,10 @@
 
 package lk.ijse.Car_Rental_Sys_api.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
 public class WebAppConfig {
 }
