319f66840825f5732410c74aec0eec736a1d734b
oshadaera68 <mindartlk010@gmail.com>
2022-07-26 11:38:19 +0530

    updated content

--- src/main/java/lk/ijse/Car_Rental_Sys_api/entity/OrderDriver_PK.java
+++ src/main/java/lk/ijse/Car_Rental_Sys_api/entity/OrderDriver_PK.java
@@ -1,4 +1,15 @@
 package lk.ijse.Car_Rental_Sys_api.entity;
 
-public class OrderDriver_PK {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class OrderDriver_PK implements Serializable {
+    private String cId;
+    private String id;
+
 }
